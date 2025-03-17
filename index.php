623180eb8ce2a5fe780e62b14b8fdf5a2004be0f
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2025-03-17 10:54:05 -0300

    Aula 17-03-2025 sobre git e banco de dados

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
-
-    echo "Olá mundo!!";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="resultado.php">
+        N1: <input type="number" name="n1">
+        <br><br>
+        N2: <input type="number" name="n2">
+        <br><br>
+        <input type="submit" value="Somar">
+    </form>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,8 @@
+<?php
+
+    $numero1 = $_REQUEST['n1'];
+    $numero2 = $_REQUEST['n2'];
+
+    echo $numero1 + $numero2;
+
+?>
