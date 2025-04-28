9b6569e63e4c6f220776283515489571fecae5f4
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2025-04-28 09:12:46 -0300

    adicionando um template bootstrap

--- script.sql
+++ script.sql
@@ -1,18 +1,13 @@
-	CREATE database projeto;
-	use projeto;
+DROP TABLE IF EXISTS `aluno`;
 
-	CREATE TABLE `projeto`.`aluno` (
-	  `id` INT NOT NULL AUTO_INCREMENT,
-	  `nome` VARCHAR(100) NOT NULL,
-	  `ra` INT NULL,
-	  `email` VARCHAR(45) NOT NULL,
-	  PRIMARY KEY (`id`));
-	  
-	INSERT INTO aluno (nome, ra, email) VALUES
-					  ('Molina', 12345, 'francisco.m.duarte@gmail.com');
-	INSERT INTO aluno (nome, ra, email) VALUES
-					  ('José', null, 'jose@gmail.com');
-	INSERT INTO aluno (nome, ra, email) VALUES
-					  ('Maria', 55555, 'maria@gmail.com');
-	INSERT INTO aluno (nome, ra, email) VALUES
-					  ('Pedro', 6666, 'pedro@gmail.com');
+CREATE TABLE `aluno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `ra` int DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `aluno` VALUES (9,'bbbb',45647,'bbb@gmail.com','e10adc3949ba59abbe56e057f20f883e'),(10,'aaaa',12123,'aaa@gmail.com','e10adc3949ba59abbe56e057f20f883e'),(14,'teste',1231312,'erasd@asd.com','e10adc3949ba59abbe56e057f20f883e');
+
